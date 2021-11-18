074f21799717ebcdeb62aa60d77e7c20cbabc0d4
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2021-11-18 16:11:02 -0600

    Create Two-Stick-Drive-Python-copy.py

---
+++ Robot mesh/Example-projects/Two-Stick-Drive-Python-copy.py
@@ -0,0 +1,17 @@
+# VEX V5 Python Project
+import sys
+import vex
+from vex import *
+
+#region config
+brain       = vex.Brain()
+motor_right = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, True)
+motor_left  = vex.Motor(vex.Ports.PORT16, vex.GearSetting.RATIO18_1, False)
+con         = vex.Controller(vex.ControllerType.PRIMARY)
+#endregion config
+
+while True:
+  # Left motor, vertical axis of left joystick
+  motor_left.spin(vex.DirectionType.FWD, (con.axis3.position(vex.PercentUnits.PCT)), vex.VelocityUnits.PCT)
+  # Right motor, vertical axis of right joystick
+  motor_right.spin(vex.DirectionType.FWD, (con.axis2.position(vex.PercentUnits.PCT)), vex.VelocityUnits.PCT)
