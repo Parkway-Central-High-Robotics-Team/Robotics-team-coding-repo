0126c12fee8730ac48eddd1f59f21d90745c8b8c
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2021-11-23 15:36:41 -0600

    Create AS-11.6.21-Competition.cpp

---
+++ Robot mesh/11.6.21/AS-11.6.21-Competition.cpp
@@ -0,0 +1,261 @@
+// VEX V5 C++ Project with Competition Template
+#include "vex.h"
+using namespace vex;
+
+//#region config_globals
+vex::brain      Brain;
+vex::motor      left_drive_mtr(vex::PORT1, vex::gearSetting::ratio18_1, false);
+vex::motor      right_drive_mtr(vex::PORT2, vex::gearSetting::ratio18_1, true);
+vex::motor      conveyor_mtr(vex::PORT3, vex::gearSetting::ratio18_1, false);
+vex::motor      lift_mtr(vex::PORT4, vex::gearSetting::ratio18_1, false);
+vex::limit      limit_A(Brain.ThreeWirePort.A);
+vex::bumper     bumper_B(Brain.ThreeWirePort.B);
+vex::pot        pot_C(Brain.ThreeWirePort.C);
+vex::limit      forklift_limit(Brain.ThreeWirePort.D);
+vex::drivetrain dt(left_drive_mtr, right_drive_mtr, 319.1764, 292.1, 0, vex::distanceUnits::mm, 1);
+vex::controller con(vex::controllerType::primary);
+//#endregion config_globals
+bool conveyor_run_up = false;
+bool conveyor_run_down = false;
+bool button_held_R1 = false;
+bool button_held_R2 = false;
+
+// Creates a competition object that allows access to Competition methods.
+vex::competition Competition;
+
+//////start of conveyor functions//////
+void conveyor_intake() {
+    conveyor_mtr.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+}
+
+void conveyor_expel() {
+    conveyor_mtr.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+}
+
+void conveyor_intake_time(double time, double velocity) {
+    conveyor_mtr.spinFor(vex::directionType::rev, time, vex::timeUnits::sec, velocity, vex::velocityUnits::pct);
+}
+
+void conveyor_expel_time(double time, double velocity) {
+    conveyor_mtr.spinFor(vex::directionType::fwd, time, vex::timeUnits::sec, velocity, vex::velocityUnits::pct);
+}
+
+void conveyor_intake_deg(double deg, double velocity) {
+    conveyor_mtr.spinFor(vex::directionType::rev, deg, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct);
+}
+
+void conveyor_expel_deg(double deg, double velocity) {
+    conveyor_mtr.spinFor(vex::directionType::fwd, deg, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct);
+}
+
+void conveyor_stop() {
+    conveyor_mtr.stop(vex::brakeType::hold);
+}
+/////end of conveyor functions/////
+
+/////start of lift functions/////
+
+void lift_up() {
+    lift_mtr.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+}
+
+void lift_down() {
+    lift_mtr.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+}
+
+void lift_up_time(double time, double velocity) {
+    lift_mtr.spinFor(vex::directionType::fwd, time, vex::timeUnits::sec, velocity, vex::velocityUnits::pct);
+}
+
+void lift_down_time(double time, double velocity) {
+    lift_mtr.spinFor(vex::directionType::rev, time, vex::timeUnits::sec, velocity, vex::velocityUnits::pct);
+}
+
+void liftUp_deg(double deg, double velocity){
+    lift_mtr.rotateFor(vex::directionType::fwd, deg, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct);
+}
+
+void liftDown_deg(double deg, double velocity){
+    lift_mtr.rotateFor(vex::directionType::rev, deg, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct);
+}
+
+void liftStopCoast(){
+    left_drive_mtr.stop(brakeType::coast);
+}
+
+void liftStopHold(){
+    left_drive_mtr.stop(brakeType::hold);
+}
+/////end of lift functions/////
+
+/////start of drivetrain functions/////
+void driveTrainStopCoast() {
+    left_drive_mtr.stop(brakeType::coast);
+    right_drive_mtr.stop(brakeType::coast);
+}
+
+void driveTrainStopHold() {
+    left_drive_mtr.stop(brakeType::hold);
+    right_drive_mtr.stop(brakeType::hold);
+}
+/////end of drivetrain functions/////
+
+/////start of button functions/////
+
+//function for button R1
+void conveyor_lift_R1_button_press() {
+    Brain.Screen.print("This works 1");
+    while(true) {
+        if(con.ButtonR1.pressing()==true){
+            button_held_R1 = true;
+            }
+        if(con.ButtonR1.pressing()==false && button_held_R1==true){
+            conveyor_run_down = !conveyor_run_down;
+            button_held_R1 = false;
+        } 
+    }
+}
+
+//function for button R2
+void conveyor_lift_R2_button_press() {
+    Brain.Screen.print("This works 1");
+    while(true) {
+        if(con.ButtonR2.pressing()==true){
+            button_held_R2 = true;
+            }
+        if(con.ButtonR2.pressing()==false && button_held_R2==true){
+            conveyor_run_up = !conveyor_run_up;
+            button_held_R2 = false;
+        } 
+    }
+}
+/////end of button functions/////
+
+/////start of threads/////
+void lift_thread() {
+    while(true) {
+        if(con.ButtonL1.pressing()==true){
+            lift_mtr.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);
+            }
+        else if(con.ButtonL2.pressing()==true){
+            lift_mtr.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);
+        }else {
+            lift_mtr.stop(vex::brakeType::hold);
+        }
+    }
+}
+
+//runs conveyor thread bassed on the bools dictated by the conveyor_lift_R2_button_press() and the conveyor_lift_R1_button_press()
+void conveyor_thread(){
+    while (true) {
+        if (conveyor_run_up ==  true) {
+            conveyor_mtr.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+            conveyor_run_down = false;
+        }else if (conveyor_run_down ==  true) {
+            conveyor_mtr.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
+            conveyor_run_up = false;
+        }else {
+            conveyor_mtr.stop(vex::brakeType::hold);
+        }
+    }
+}
+/////end of threads/////
+
+//autonomus functions
+void auton_test() {
+    //place robot facing backwards against the wall with 3 preeloads. Make sure it is in line with with yellow mobile goal. The robot will lower the lift, go backwards, latch onto goal, pull the goal to our side, and turn on the conveyor to eject the preeloads into the mobile goal.
+    liftDown_deg(290,60);
+    dt.driveFor(vex::directionType::rev, 50, vex::distanceUnits::in, 65, vex::velocityUnits::pct, true);
+    liftUp_deg(25,40);
+    dt.driveFor(vex::directionType::fwd, 45, vex::distanceUnits::in, 50, vex::velocityUnits::pct, true);
+    dt.turnFor(vex::turnType::right, 3, vex::rotationUnits::deg, 40, vex::velocityUnits::pct, true);
+    conveyor_mtr.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
+    sleepMs(3000);
+    conveyor_mtr.stop(vex::brakeType::hold);
+}
+
+void auton_donotuseyet() {
+    dt.driveFor(vex::directionType::fwd, 4, vex::distanceUnits::in, 100, vex::velocityUnits::pct, true);
+    dt.turnFor(vex::turnType::right, 30, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
+    conveyor_intake();
+    dt.driveFor(vex::directionType::fwd, 10, vex::distanceUnits::in, 100, vex::velocityUnits::pct, true);
+    dt.turnFor(vex::turnType::left, 30, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
+    dt.driveFor(vex::directionType::fwd, 10, vex::distanceUnits::in, 100, vex::velocityUnits::pct, true);
+    conveyor_stop();
+    liftDown_deg(90,40);
+    dt.turnFor(vex::turnType::left, 20, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
+    dt.driveFor(vex::directionType::rev, 10, vex::distanceUnits::in, 100, vex::velocityUnits::pct, true);
+    liftUp_deg(45,40);
+    dt.turnFor(vex::turnType::left, 60, vex::rotationUnits::deg, 100, vex::velocityUnits::pct, true);
+    dt.driveFor(vex::directionType::fwd, 10, vex::distanceUnits::in, 100, vex::velocityUnits::pct, true);
+    dt.stop(vex::brakeType::coast);
+    conveyor_intake();
+    sleepMs(2000);
+}
+
+/////start of offical vex functions. (do not revove these)/////
+void pre_auton() {
+    // All activities that occur before competition start
+    // Example: setting initial position
+}
+
+void autonomous() {
+    // Place autonomous code here
+}
+
+void drivercontrol() {
+    // Place drive control code here, inside the loop
+    while (true) {
+        // This is the main loop for the driver control.
+        // Each time through the loop you should update motor
+        // movements based on input from the controller.
+        //thread call
+        thread m_conveyor_lift_R1_button_press = thread(conveyor_lift_R1_button_press);
+        thread m_conveyor_lift_R2_button_press = thread(conveyor_lift_R2_button_press);
+        thread m_conveyor_thread = thread(conveyor_thread);
+        thread m_lift_thread = thread(lift_thread);
+        //one stick drive 
+        /*
+        float max = 200;
+        float left_percent = con.Axis2.value()/max;
+        float right_percent = con.Axis2.value()/max;
+           
+        float motor_max = 100;
+        int left_power = left_percent * motor_max;
+        int right_power = right_percent * motor_max;
+        
+        left_drive_mtr.spin(vex::directionType::fwd, left_power, vex::velocityUnits::pct); 
+        right_drive_mtr.spin(vex::directionType::fwd, right_power, vex::velocityUnits::pct);
+        
+        wait(20,timeUnits::msec);
+        */
+        
+        //tank drive
+        float max = 200;
+        float left_percent = con.Axis3.value()/max;
+        float right_percent = con.Axis2.value()/max;
+           
+        float motor_max = 100;
+        int left_power = left_percent * motor_max;
+        int right_power = right_percent * motor_max;
+        
+        left_drive_mtr.spin(vex::directionType::fwd, left_power, vex::velocityUnits::pct); 
+        right_drive_mtr.spin(vex::directionType::fwd, right_power, vex::velocityUnits::pct);
+        
+        
+        wait(20,timeUnits::msec);
+    }
+}
+
+int main() {
+    // Do not adjust the lines below
+    Brain.Screen.print("User Program has Started.");
+    // Set up (but don't start) callbacks for autonomous and driver control periods.
+    Competition.autonomous(auton_test);
+    Competition.drivercontrol(drivercontrol);
+    // Run the pre-autonomous function.
+    pre_auton();
+
+    // Robot Mesh Studio runtime continues to run until all threads and competition callbacks are finished.
+}
+/////end of offical vex functions. (do not revove these)/////
