ab13dc49001a8083db288dbba2791070b25c6e40
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2021-12-02 16:13:17 -0600

    Add files via upload

---
+++ VexV5Code/12.2.21/AS-12.2.21-2021-12-02T21-53-17/include/robot-config.h
@@ -0,0 +1,16 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern smartdrive Drivetrain;
+extern motor lift;
+extern motor arm;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
