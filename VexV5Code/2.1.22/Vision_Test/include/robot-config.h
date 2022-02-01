1914d790305387dfd7f0e700fd3cf28dd3c3198c
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-02-01 16:16:18 -0600

    Add files via upload

---
+++ VexV5Code/2.1.22/Vision_Test/include/robot-config.h
@@ -0,0 +1,24 @@
+using namespace vex;
+
+extern brain Brain;
+
+using signature = vision::signature;
+
+// VEXcode devices
+extern smartdrive Drivetrain;
+extern controller Controller1;
+extern signature Vision__SIG_1;
+extern signature Vision__SIG_2;
+extern signature Vision__SIG_3;
+extern signature Vision__SIG_4;
+extern signature Vision__SIG_5;
+extern signature Vision__SIG_6;
+extern signature Vision__SIG_7;
+extern vision Vision;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
