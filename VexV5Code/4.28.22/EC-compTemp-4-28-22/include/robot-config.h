f9b7a185522c29e46255e7721663ec252520356a
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-04-28 14:34:58 -0500

    created 4.28.22

---
+++ VexV5Code/4.28.22/EC-compTemp-4-28-22/include/robot-config.h
@@ -0,0 +1,31 @@
+#pragma once
+using namespace vex;
+
+#include "v5.h"
+
+extern brain Brain;
+
+using signature = vision::signature;
+
+// VEXcode devices
+extern vision Vision;
+
+// VEXcode devices
+extern smartdrive Drivetrain;
+extern controller Controller1;
+extern inertial DrivetrainInertial;
+extern motor leftMotorA;
+extern motor leftMotorB;
+extern motor rightMotorA; 
+extern motor rightMotorB; 
+extern motor Quick_grab;
+extern motor lift_front;
+extern motor claw_back;
+extern motor lift_clamp;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
