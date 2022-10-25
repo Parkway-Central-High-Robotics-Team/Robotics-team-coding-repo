de282f0d58138f312cf0e8013e27934296cb9a6f
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-10-25 15:10:05 -0500

    Created 10.25.22

---
+++ VexV5Code/10.25.22/10.25.22Training/include/pre-defined-functions.h
@@ -0,0 +1,13 @@
+//#ifndef PRE_DEFINED_FUNCTIONS
+//#define PRE_DEFINED_FUNCTIONS
+#pragma once
+using namespace vex;
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+#include "v5.h"
+#include "v5_vcs.h"
