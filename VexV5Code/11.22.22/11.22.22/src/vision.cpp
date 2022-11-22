1acab03a911dc651a44758e92326a69b576c422e
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-11-22 16:21:22 -0600

    created 11.22.22

---
+++ VexV5Code/11.22.22/11.22.22/src/vision.cpp
@@ -0,0 +1,130 @@
+//#include "vex.h"
+#include "vision.h"
+using namespace vex;
+
+/*bool blueTest(void) {
+  Vision1.takeSnapshot(SIG_BLUE);
+  if(Vision1.largestObject.exists){
+    return true;
+  }
+  return false;
+}
+
+bool redTest(void) {
+  Vision1.takeSnapshot(SIG_RED);
+  if(Vision1.largestObject.exists){
+    return true;
+  }
+  return false;
+}
+
+int spinnerPos(void) {
+  bool blueExist = blueTest();
+  bool redExist = redTest();
+  if(blueExist && !redExist){
+    return 1;
+  }else if (redExist && !blueExist) {
+    return 2;
+  }else if (redExist && blueExist) {
+    return 3;
+  }
+  return 0;
+}
+
+void spinnerBlueToRed(void){
+  intake.spinFor(forward, 50, rotationUnits::deg);
+}
+
+void spinnerRedToBlue(void){
+  intake.spinFor(reverse, 50, rotationUnits::deg);
+}
+
+void spinnerInbetween(void){
+
+}
+
+void visionTest(void){
+  if((int)spinnerPos == 1){
+    spinnerBlueToRed();
+    Brain.Screen.print("spinnerBlueToRed");
+  }else if ((int)spinnerPos == 2) {
+    spinnerRedToBlue();
+    Brain.Screen.print("spinnerRedToBlue");
+  }else if ((int)spinnerPos == 3) {
+    spinnerInbetween();
+    Brain.Screen.print("spinnerInbetween");
+  }
+}*/
+
+int returner = 0;
+
+void visionTester(void){
+    Vision1.takeSnapshot(SIG_RED);
+    if(Vision1.largestObject.exists){
+      intake.spinFor(forward, 50, rotationUnits::deg);
+    }
+}
+
+void visionTester1(void){
+    int bright = Vision1.getBrightness();
+    Brain.Screen.print(bright);
+}
+
+bool blueTest(void) {
+  Vision1.takeSnapshot(SIG_BLUE);
+  return Vision1.largestObject.exists;
+}
+
+bool redTest(void) {
+  Vision1.takeSnapshot(SIG_RED);
+  return Vision1.largestObject.exists;
+}
+
+bool spinnerPos(void) {
+  bool blueExist = blueTest();
+  bool redExist = redTest();
+  if(blueExist && redExist == false){
+    Brain.Screen.print("Returned: 1");
+    Brain.Screen.newLine();
+    return returner;
+  }else if (redExist && blueExist == false) {
+    Brain.Screen.print("Returned: 2");
+    Brain.Screen.newLine();
+    return returner;
+  }else if (redExist && blueExist) {
+    Brain.Screen.print("Returned: 3");
+    Brain.Screen.newLine();
+    return returner;
+  }
+  Brain.Screen.print("Returned: 0");
+  Brain.Screen.newLine();
+  return returner;
+}
+
+void spinnerBlueToRed(void){
+  intake.spinFor(forward, 50, rotationUnits::deg);
+}
+
+void spinnerRedToBlue(void){
+  intake.spinFor(reverse, 50, rotationUnits::deg);
+}
+
+void spinnerInbetween(void){
+
+}
+
+void visionTest(void){
+    spinnerPos();
+    if((int)returner == 1){
+        spinnerBlueToRed();
+        Brain.Screen.print("spinnerBlueToRed");
+        Brain.Screen.newLine();
+    }else if ((int)returner == 2) {
+        spinnerRedToBlue();
+        Brain.Screen.print("spinnerRedToBlue");
+    }else if ((int)returner == 3) {
+        spinnerInbetween();
+        Brain.Screen.print("spinnerInbetween");
+        Brain.Screen.newLine();
+  }
+}
