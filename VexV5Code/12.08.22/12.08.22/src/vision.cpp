004a9d580ee7164793faf89a27ab22c0e97f26b5
targed <69736930+targed@users.noreply.github.com>
2022-12-09 11:52:18 -0800

    Update vision.cpp

--- VexV5Code/12.08.22/12.08.22/src/vision.cpp
+++ VexV5Code/12.08.22/12.08.22/src/vision.cpp
@@ -66,13 +66,13 @@ void spinnerPos(void) {
     if(!redExist && blueExist){
       intake.stop(brakeType::hold);
     }else {
-      intake.spin(forward, 35, velocityUnits::pct);
+      intake.spin(forward, 25, velocityUnits::pct);
     }
   }else if (side == blueSide) {
     if(redExist && !blueExist){
       intake.stop(brakeType::hold);
     }else {
-      intake.spin(forward, 35, velocityUnits::pct);
+      intake.spin(forward, 25, velocityUnits::pct);
     }
   }
 }
