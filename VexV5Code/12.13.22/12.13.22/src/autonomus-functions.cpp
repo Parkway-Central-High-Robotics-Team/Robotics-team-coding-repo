dc3a44ae3bc16c404bd0e685140fda499e539acc
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2022-12-13 16:10:48 -0600

    Tried vision stuff and created 12.13.22

---
+++ VexV5Code/12.13.22/12.13.22/src/autonomus-functions.cpp
@@ -0,0 +1,110 @@
+#include "vex.h"
+#include "autonomous-functions.h"
+
+using namespace vex;
+void moveForward(int x){
+  Drivetrain.drive(fwd, 100, velocityUnits::pct);
+  wait(x, timeUnits::msec);
+  Drivetrain.stop(brake);
+  //wait(milliseconds, timeUnits::msec);
+  
+}
+
+void moveBackward(int x){
+  Drivetrain.drive(reverse, 100, velocityUnits::pct);
+  wait(x, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+  
+
+void turnRight(int x){
+  Drivetrain.turn(right);
+  wait(x, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void turnLeft(int x){
+  Drivetrain.turn(left);
+  wait(x, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void expansion(void){
+  //enter in pneumatic code from "pneumaticstestv3"
+  Pneumatics.set(false);
+  if(Pneumatics.value() == 0){
+      Pneumatics.set(true);
+      Brain.Screen.print("L1 is Pressing");
+    }
+
+
+}
+
+void roller(int x){
+  intake.spinFor(forward, x, timeUnits::msec, 100, velocityUnits::pct);
+ 
+}
+
+
+void auton_function_test(void) {
+  //roller first
+  roller(500);
+  //wait(100, timeUnits::msec);
+  // go forward, turn right, move backward
+  moveForward(1000);
+  turnRight(2190);
+  moveBackward(1125);
+  roller(500);
+  moveForward(500);
+  turnRight(3000);
+  moveBackward(6050);
+  turnLeft(1300);
+  moveBackward(1250);
+  roller(500);
+  moveForward(1250);
+  turnRight(2000);
+  moveBackward(1300);
+  roller(500);
+  moveForward(1000);
+  turnRight(100);
+  //wait();
+  //expansion();
+  
+
+
+  
+  /*turnRight(1000);
+  moveBackward(1000);
+  // roller 
+  //turn right until parallel with divison
+  turnRight(1000);
+  //move forward to top left roller
+  moveForward(1000);
+  //turn left
+  turnLeft(1000);
+  //roller
+  //move backward, turn right
+  moveBackward(1000);
+  turnRight(1000);
+  // roller
+  //move out and expand
+  */
+
+}
+
+void auton_skills(void){
+
+}
+
+void regular_auton(void) {
+  intake.spinFor(forward, 15, rotationUnits::deg, 15, velocityUnits::pct);
+  Drivetrain.driveFor(forward, 5, distanceUnits::in, 50, velocityUnits::pct);
+  Drivetrain.turnFor(right, 120, rotationUnits::deg, 50, velocityUnits::pct);
+  Drivetrain.driveFor(forward, 50, distanceUnits::in, 50, velocityUnits::pct);
+  startFlyWheel(100);
+  Drivetrain.turnFor(right, 90, rotationUnits::deg, 50, velocityUnits::pct);
+  intakeIn(100);
+  wait(3000, msec);
+  intakeStop();
+  stopFlyWheel();
+}
