4e9ba05ccd469f2621e493a6684e534eb30481ed
kaiserl4961 <44265457+kaiserl4961@users.noreply.github.com>
2023-01-27 16:40:15 -0600

    created 1.27.23

---
+++ VexV5Code/2022-2023/1.27.23/1.27.23/src/autonomus-functions.cpp
@@ -0,0 +1,89 @@
+#include "vex.h"
+#include "autonomous-functions.h"
+
+using namespace vex;
+
+void auton_function_test(void) {
+  rollerTime(500);
+  moveForwardTime(600);
+  turnRightTime(1050);
+  moveBackwardTime(750);
+  rollerTime(500);
+  moveForwardTime(300);
+  turnLeftTime(500);
+  moveForwardTime(3800);
+  turnRightTime(1450);
+  moveBackwardTime(850);
+  rollerTime(750);
+  moveForwardTime(600);
+  turnRightTime(1100);
+  moveBackwardTime(700);
+  rollerTime(500);
+
+  
+  /*
+  //roller first
+  roller(500);
+  wait(100, timeUnits::msec);
+  // go forward, turn right, move backward
+  moveForward(500);
+  turnRight(1450);
+  moveBackward(780);
+  roller(500);
+  moveForward(300);
+  turnRight(2000);
+
+
+
+  
+  turnRight(1000);
+  moveBackward(1000);
+  // roller 
+  //turn right until parallel with divison
+  turnRight(1000);
+  //move forward to top left roller
+  moveForward(1000);
+  //turn left
+  turnLeft(1000);
+  //roller
+  //move backward, turn right
+  moveBackward(1000);
+  turnRight(1000);
+  // roller
+  //move out and expand
+  */
+}
+
+void auton_skills(void){
+  rollerTime(250);
+  //expansion(); 
+}
+
+void regular_auton(void) {
+  intake.spinFor(forward, 15, rotationUnits::deg, 15, velocityUnits::pct);
+  Drivetrain.driveFor(forward, 5, distanceUnits::in, 50, velocityUnits::pct);
+  Drivetrain.turnFor(right, 120, rotationUnits::deg, 50, velocityUnits::pct);
+  Drivetrain.driveFor(forward, 50, distanceUnits::in, 50, velocityUnits::pct);
+  startFlyWheel(100);
+  Drivetrain.turnFor(right, 90, rotationUnits::deg, 50, velocityUnits::pct);
+  intakeIn(100);
+  wait(3000, msec);
+  intakeStop();
+  stopFlyWheel();
+}
+
+void shootLowerGoal(void) {
+  //intake.spinFor(forward, 15, rotationUnits::deg, 15, velocityUnits::pct);
+  Drivetrain.driveFor(forward, 5, distanceUnits::in, 50, velocityUnits::pct);
+  Drivetrain.turnFor(right, 90, rotationUnits::deg, 50, velocityUnits::pct);
+  startFlyWheel(100);
+  wait(500, msec);
+  intakeIn(100);
+  wait(3000, msec);
+  intakeStop();
+  stopFlyWheel();
+}
+
+void roller2(void){
+  rollerTime(250);
+}
---
+++ VexV5Code/2022-2023/1.27.23/1.27.23/src/pre-defined-functions.cpp
@@ -0,0 +1,285 @@
+#include "vex.h"
+#include "pre-defined-functions.h"
+using namespace vex;
+
+void intakeStop(void) {
+  intake.stop();
+}
+
+void intakeIn(int vel){
+  intake.spin(forward, vel, velocityUnits::pct);
+}
+
+void intakeInTime(int msc, int vel){
+  intake.spinFor(forward, msc, timeUnits::msec, vel, velocityUnits::pct);
+  intake.stop();
+}
+
+void intakeInDeg(int deg, int vel){
+  intake.spinFor(forward, deg, rotationUnits::deg, vel, velocityUnits::pct);
+  intake.stop();
+}
+
+void intakeOut(int vel){
+  intake.spin(reverse, vel, velocityUnits::pct);
+}
+
+void intakeOutTime(int msc, int vel){
+  intake.spinFor(reverse, msc, timeUnits::msec, vel, velocityUnits::pct);
+  intake.stop();
+}
+
+void intakeOutDeg(int deg, int vel){
+  intake.spinFor(reverse, deg, rotationUnits::deg, vel, velocityUnits::pct);
+  intake.stop();
+}
+
+void startFlyWheel(int vel){
+  spinMtrs.spin(forward, vel, velocityUnits::pct);
+}
+
+void stopFlyWheel(void){
+  spinMtrs.stop();
+}
+
+void pneumaticsFunction(void){
+  // NEEDS TO BE SET TO FALSE IN GLOBAL CODESPACE 
+  Pneumatics.set(false);
+    if(Pneumatics.value() == 0){
+      Pneumatics.set(true);
+      Brain.Screen.print("L1 is Pressing");
+    }else if(Pneumatics.value() == 1){
+     Pneumatics.set(false);
+     Brain.Screen.print("L2 is Pressing");
+  }
+}
+
+void opticalFunction(){
+  Brain.Screen.clearScreen();
+  Brain.Screen.setCursor(1, 1);
+  Optical2.setLight(ledState::on);
+  Optical2.setLightPower(50,percent);
+
+  if (Optical2.isNearObject()){
+    Brain.Screen.print("Object Detected");
+    Brain.Screen.newLine();
+
+    Brain.Screen.print("Brightness: ");
+    Brain.Screen.print("%.2f", Optical2.brightness());
+    Brain.Screen.newLine();
+
+    Brain.Screen.print("Hue: ");
+    Brain.Screen.print("%.2f", Optical2.hue());
+    Brain.Screen.newLine();
+    if(side == redSide){
+      if(Optical2.color() == blue){
+        intake.stop(brakeType::hold);
+      }else {
+        intake.spin(forward, 15, velocityUnits::pct);
+      }
+    }else if (side == blueSide) {
+      if(Optical2.color() == red){
+        intake.stop(brakeType::hold);
+      }else {
+        intake.spin(forward, 15, velocityUnits::pct);
+      }
+    }
+  }
+}
+
+void moveForwardTime(int msc){
+  Drivetrain.drive(reverse, 75, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+  //wait(milliseconds, timeUnits::msec);
+}
+
+void moveForwardTime(int msc, int vel){
+  Drivetrain.drive(reverse, vel, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+  //wait(milliseconds, timeUnits::msec);
+}
+
+void moveForwardTime(int msc, int vel, vex::brakeType bt){
+  Drivetrain.drive(reverse, vel, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(bt);
+  //wait(milliseconds, timeUnits::msec);
+}
+
+////////////////////////////////////////////////////////////
+
+void moveBackwardTime(int msc){
+  Drivetrain.drive(forward, 65, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void moveBackwardTime(int msc, int vel){
+  Drivetrain.drive(forward, vel, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void moveBackwardTime(int msc, int vel, vex::brakeType bt){
+  Drivetrain.drive(forward, vel, velocityUnits::pct);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(bt);
+}
+
+/////////////////////////////////////////////////////////
+  
+
+void turnRightTime(int msc){
+  Drivetrain.setTurnVelocity(75, velocityUnits::pct);
+  Drivetrain.turn(right);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void turnRightTime(int msc, int vel){
+  Drivetrain.setTurnVelocity(vel, velocityUnits::pct);
+  Drivetrain.turn(right);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void turnRightTime(int msc, int vel, vex::brakeType bt){
+  Drivetrain.setTurnVelocity(vel, velocityUnits::pct);
+  Drivetrain.turn(right);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(bt);
+}
+
+/////////////////////////////////////////////////////
+
+void turnLeftTime(int msc){
+  Drivetrain.setTurnVelocity(75, velocityUnits::pct);
+  Drivetrain.turn(left);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void turnLeftTime(int msc, int vel){
+  Drivetrain.setTurnVelocity(vel, velocityUnits::pct);
+  Drivetrain.turn(left);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(brake);
+}
+
+void turnLeftTime(int msc, int vel, vex::brakeType bt){
+  Drivetrain.setTurnVelocity(vel, velocityUnits::pct);
+  Drivetrain.turn(left);
+  wait(msc, timeUnits::msec);
+  Drivetrain.stop(bt);
+}
+
+/////////////////////////////////////////////////////
+
+void rollerTime(int msc){
+  intake.spinFor(forward, msc, timeUnits::msec, 100, velocityUnits::pct);
+}
+
+void rollerTime(int msc, int vel){
+  intake.spinFor(forward, msc, timeUnits::msec, vel, velocityUnits::pct);
+}
+
+/////////////////////////////////////////////////////
+
+void expansion(void){
+  //enter in pneumatic code from "pneumaticstestv3"
+  Pneumatics.set(false);
+  if(Pneumatics.value() == 0){
+      Pneumatics.set(true);
+      Brain.Screen.print("L1 is Pressing");
+    }
+}
+
+/////////////////////////////////////////////////////
+
+void indexerFire(void){
+  intakeOutTime(150, 50);
+  indexer.setVelocity(100, velocityUnits::pct);
+  indexer.spinFor(forward, 50, rotationUnits::deg);
+  indexer.setBrake(brake);
+  intakeInTime(3000, 100);
+  indexer.spinFor(reverse, 250, timeUnits::msec);
+}
+
+/////////////////////////////////////////////////////
+  /*if (Optical2.isNearObject()) {
+    Brain.Screen.print("Object Detected");
+    Brain.Screen.newLine();
+    
+    Brain.Screen.print("Brightness: ");
+    Brain.Screen.print("%.2f", Optical2.brightness());
+    Brain.Screen.newLine();
+
+    Brain.Screen.print("Hue: ");
+    Brain.Screen.print("%.2f", Optical2.hue());
+    Brain.Screen.newLine();
+
+    Brain.Screen.print("Detects a red object?: ");
+    Brain.Screen.print("%s", Optical2.color() == red ? "TRUE" : "FALSE");
+    Brain.Screen.newLine();
+
+    Brain.Screen.print("Detects a blue object?: ");
+    Brain.Screen.print("%s", Optical2.color() == blue ? "TRUE" : "FALSE");
+    Brain.Screen.newLine();
+  } else {
+    Brain.Screen.print("No Object Detected");
+  }*/
+
+  //Optical2.setLight(ledState::off);
+
+/*
+class Intake {
+public:
+  void spin(directionType direction, int velocity, velocityUnits units) {
+    intake.spin(direction, velocity, units);
+  }
+
+  void spinFor(directionType direction, int value, unitType type, int velocity, velocityUnits units) {
+    intake.spinFor(direction, value, type, velocity, units);
+    intake.stop();
+  }
+};
+
+class FlyWheel {
+public:
+  void spin(int velocity, velocityUnits units) {
+    spinMtrs.spin(forward, velocity, units);
+  }
+
+  void stop() {
+    spinMtrs.stop();
+  }
+};
+
+class Pneumatics {
+public:
+  void set(bool value) {
+    Pneumatics.set(value);
+  }
+
+  bool value() {
+    return Pneumatics.value();
+  }
+};
+
+Intake intake;
+FlyWheel flyWheel;
+Pneumatics pneumatics;
+
+void pneumaticsFunction() {
+  pneumatics.set(false);
+  if (pneumatics.value() == 0) {
+    pneumatics.set(true);
+    Brain.Screen.print("L1 is Pressing");
+  } else if (pneumatics.value() == 1) {
+    pneumatics.set(false);
+    Brain.Screen.print("L2 is Pressing");
+  }
+}
+*/
